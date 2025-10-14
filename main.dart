37f20433bc52a8a59981607c662807b43f529cc3
HaoDuong1994 <hao.duong@egitech.vn>
2025-10-14 20:09:14 +0700

    practice extend today

--- main.dart
+++ main.dart
@@ -14,8 +14,18 @@ void main() {
 
 // Lưu Ý
 //Những biến bắt đầu bằng _ sẽ là biến private. Ở file khác sẽ không truy cập được nên cần phải có biến getter
+
 //Biến nào ko muốn để vào contructor thì có thể chỉ định giá trị mặc định
+
 //Getter và Setter dùng trong trường hợp 
+
 //Lưu ý thuộc tính nào mà chỉ định final thì sẽ ko đc thay đỗi nữa
-//1) Khi muốn bóc tách dữ liệu ra khỏi thời gian thực
+
+//khi muôn thêm xử lý logic  trước khi nhận giá trị có thể thêm logic vào hàm get 
+
+
+//Lưu ý khi dùng kế thừa
+//Cha có contructor mặc định không tham số. Thì ở class con ko cần gọi super
+//Cha chỉ có contructor tham số bắt buộc gọi super()
+//Mục đích việc gọi super là để chạy hết code của cha để code con có thể kế thừa đc giá trị và phương thức từ cha
 
